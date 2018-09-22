e2224d203531ee4db6866f5c9f6bbfb39b95e631
Carla Wilby <carla@zelda.org.za>
2018-09-22 14:54:35 +0200

    Enabled firestore add new message

--- firebase_service.py
+++ firebase_service.py
@@ -1,12 +1,15 @@
-import firebase
 import datetime
+from google.cloud import firestore
 
-db = firebase.database()
+db = firestore.Client()
 
 
 def push_message(user_id, response):
-    db.child(user_id).push({
-        "messageFromBot": True,
-        "messageSendTime": "" + datetime.datetime.now(),
-        "messageText": "" + response
+    db.collection(user_id).add({
+        u"messageFromBot": True,
+        u"messageSendTime": datetime.datetime.now(),
+        u"messageText": response
     })
+
+
+push_message("56iEUgkELJ8B3rYqDpyB", "Hello")
