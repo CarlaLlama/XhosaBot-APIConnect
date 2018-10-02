6674132919a9eeea553741f6f60494bb2ef22fa3
Carla Wilby <carla@zelda.org.za>
2018-10-02 21:45:37 +0200

    Refactored the training samples

--- api_trainer.py
+++ api_trainer.py
@@ -23,170 +23,17 @@ create_details = feersum_nlu.FaqMatcherCreateDetails(name=instance_name,
                                                      lid_model_file="lid_za",
                                                      load_from_store=False)
 
-# The training samples.
-labelled_phrases_list = []
-# 1: Why is my baby not sleeping?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Kutheni umntwana wam engalali?",
-                                                            label="lala",
-                                                            lang_code="xho"))
-
-# 2: How do I register my baby?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndingambhalisa njani umntwana wam?",
-                                                            label="bhala",
-                                                            lang_code="xho"))
-
-# 3: Signs I am in labour
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Impawu zokuba ndizokubeleka?",
-                                                            label="zokubeleka",
-                                                            lang_code="xho"))
-
-# 4: How do I know if I am having premature labour?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndazi kanjani ukuba ndizokubeleka phambi kwexesha?",
-                                                            label="phambi",
-                                                            lang_code="xho"))
-
-# 5: I have pre-eclampsia
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndine pre-eclampsia?",
-                                                            label="pre-eclampsia",
-                                                            lang_code="xho"))
-
-# 6: How long should labour last?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Lide kangakanani ixesha lokubeleka?",
-                                                            label="ixhesha",
-                                                            lang_code="xho"))
-
-# 7: I am having morning sickness
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndinesifo sasekuseni?",
-                                                            label="morning_sickness",
-                                                            lang_code="xho"))
-
-# 8: I have a UTI during pregnancy
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndine UTI ngelixa ndikhulelwe",
-                                                            label="uti",
-                                                            lang_code="xho"))
-
-# 9: High-blood pressure during pregnancy
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="I high-blood pressure ngelixa ukhulelwe",
-                                                            label="high-blood",
-                                                            lang_code="xho"))
-
-# 10: I have an STD
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndinesifo sokwasulelana ngesondo",
-                                                            label="std",
-                                                            lang_code="xho"))
-
-# 11: I am sick during pregnancy
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndiyagula ngelixa ndikhulelwe",
-                                                            label="ndiyagula",
-                                                            lang_code="xho"))
-
-# 12: My baby is sick
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Umntwana wam uyagula",
-                                                            label="uyagula",
-                                                            lang_code="xho"))
-
-# 13: I am in pain, how do I get to a clinic
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndineentlungu, ndingaya njani ekliniki?",
-                                                            label="ekliniki",
-                                                            lang_code="xho"))
-
-# 14: I have swollen legs
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndinemilenze edumbileyo",
-                                                            label="dumbileyo",
-                                                            lang_code="xho"))
-
-# 15: What must I feed my baby?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Yintoni endimele ndondle ngayo umntwana wam?",
-                                                            label="ndondle",
-                                                            lang_code="xho"))
-
-# 16: I am bleeding during pregnancy
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndiyopha ngexesha lokukhulelwa",
-                                                            label="opha",
-                                                            lang_code="xho"))
-
-# 17: I am depressed
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndinocinezelelo lwengqondo",
-                                                            label="depressed",
-                                                            lang_code="xho"))
-
-
-# 18: How do I get a grant for my baby?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndingasifumana njani isibonelelo somntwana wam?",
-                                                            label="isibonelelo",
-                                                            lang_code="xho"))
-
-# 19: Will my baby have HIV?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ingaba umntwana wam uzobane HIV?",
-                                                            label="hiv",
-                                                            lang_code="xho"))
-
-
-# 20: Can I breastfeed with HIV?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndingancancisa ndine HIV?",
-                                                            label="hiv_cancisa",
-                                                            lang_code="xho"))
-
-# 21: My baby has nappy rash
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Umntwana wam unorhawuzelelo owenziwa yi-nappy",
-                                                            label="nappy_rash",
-                                                            lang_code="xho"))
-
-# 22: My baby is not eating
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Umntwana wam akatyi",
-                                                            label="akatyi",
-                                                            lang_code="xho"))
-
-# 23: Can I have sex during pregnancy?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndingabelana ngesondo xa dikhulelwe?",
-                                                            label="ngesondo",
-                                                            lang_code="xho"))
-
-# 24: Should I have a C Section?
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndingayenza I C-Section?",
-                                                            label="c-section",
-                                                            lang_code="xho"))
-
-labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Ndingayenza uqhaqho?",
-                                                            label="c-section",
-                                                            lang_code="xho"))
-
-
-
-
-
-
-
-labelled_text_sample_testing_list = []
-labelled_text_sample_testing_list.append(feersum_nlu.LabelledTextSample(text="Where do I claim?",
-                                                                        label="claim",
-                                                                        lang_code="eng"))
-labelled_text_sample_testing_list.append(feersum_nlu.LabelledTextSample(text="Waar moet ek eis?",
-                                                                        label="claim",
-                                                                        lang_code="afr"))
-
-labelled_text_sample_testing_list.append(feersum_nlu.LabelledTextSample(text="Can I put in a claim?",
-                                                                        label="quote"))  # text actually on 'claim'.
-labelled_text_sample_testing_list.append(feersum_nlu.LabelledTextSample(text="Waar kan ek 'n prys kry?",
-                                                                        label="quote"))
-
-additional_labelled_text_sample_list = []
-additional_labelled_text_sample_list.append(feersum_nlu.LabelledTextSample(text="How much does a quote cost?",
-                                                                           label="quote"))
-additional_labelled_text_sample_list.append(feersum_nlu.LabelledTextSample(text="How long does a claim take?",
-                                                                           label="claim"))
 
 word_manifold_list = [feersum_nlu.LabeledWordManifold('eng', 'feers_wm_eng'),
                       feersum_nlu.LabeledWordManifold('afr', 'feers_wm_afr'),
                       feersum_nlu.LabeledWordManifold('zul', 'feers_wm_zul')]
-
+labelled_phrases_list.append(feersum_nlu.LabelledTextSample(text="Kutheni umntwana wam engalali?",
+                                                            label="lala",
+                                                            lang_code="xho"))
 
 faq_matcher_get_labels
 
-text_input_0 = feersum_nlu.TextInput("Waar kan ek 'n eis insit?")
-text_input_1 = feersum_nlu.TextInput("How long does a claim take?")
 
-print()
 
 try:
     print("Create the FAQ matcher:")
---
+++ dataset_text_samples.py
@@ -0,0 +1,105 @@
+
+# The training samples.
+# Format: [Phrase, Label, Language code]
+
+labelled_phrases_list = [
+    # Phrase 1:
+    ["Kutheni umntwana wam engalali?", "lala", "xho"],
+    ["Why is my baby not sleeping?", "lala", "eng"],
+
+    # Phrase 2:
+    ["Ndingambhalisa njani umntwana wam?", "bhala", "xho"],
+    ["How do I register my baby?", "bhala", "eng"],
+
+    # Phrase 3:
+    ["Impawu zokuba n dizokubeleka?", "zokubeleka", "xho"],
+    ["Signs I am in labour", "zokubeleka", "eng"],
+
+    # Phrase 4:
+    ["Ndazi kanjani ukuba ndizokubeleka phambi kwexesha?", "phambi", "xho"],
+    ["How do I know if I am having premature labour?", "phambi", "eng"],
+
+    # Phrase 5:
+    ["Ndine pre-eclampsia?", "pre-eclampsia", "xho"],
+    ["I have pre-eclampsia", "pre-eclampsia", "eng"],
+
+    # Phrase 6:
+    ["Lide kangakanani ixesha lokubeleka?", "ixhesha", "xho"],
+    ["How long should labour last?", "ixhesha", "eng"],
+
+    # Phrase 7:
+    ["Ndinesifo sasekuseni?", "morning_sickness", "xho"],
+    ["I am having morning sickness", "morning_sickness", "eng"],
+
+    # Phrase 8:
+    ["Ndine UTI ngelixa ndikhulelwe", "uti", "xho"],
+    ["I have a UTI during pregnancy", "uti", "eng"],
+
+    # Phrase 9:
+    ["I high-blood pressure ngelixa ukhulelwe", "high-blood", "xho"],
+    ["High-blood pressure during pregnancy", "high-blood", "eng"],
+
+    # Phrase 10:
+    ["Ndinesifo sokwasulelana ngesondo", "std", "xho"],
+    ["I have an STD", "std", "eng"],
+
+    # Phrase 11:
+    ["Ndiyagula ngelixa ndikhulelwe", "ndiyagula", "xho"],
+    ["I am sick during pregnancy", "ndiyagula", "eng"],
+
+    # Phrase 12:
+    ["Umntwana wam uyagula", "uyagula", "xho"],
+    ["My baby is sick", "uyagula", "eng"],
+
+    # Phrase 13:
+    ["Ndineentlungu, ndingaya njani ekliniki?", "ekliniki", "xho"],
+    ["I am in pain, how do I get to a clinic", "ekliniki", "eng"],
+
+    # Phrase 14:
+    ["Ndinemilenze edumbileyo", "dumbileyo", "xho"],
+    ["I have swollen legs", "dumbileyo", "eng"],
+
+    # Phrase 15:
+    ["Yintoni endimele ndondle ngayo umntwana wam?", "ndondle", "xho"],
+    ["What must I feed my baby?", "ndondle", "eng"],
+
+    # Phrase 16:
+    ["Ndiyopha ngexesha lokukhulelwa", "opha", "xho"],
+    ["I am bleeding during pregnancy", "opha", "eng"],
+
+    # Phrase 17:
+    ["Ndinocinezelelo lwengqondo", "depressed", "xho"],
+    ["I am depressed", "depressed", "eng"],
+
+    # Phrase 18:
+    ["Ndingasifumana njani isibonelelo somntwana wam?", "isibonelelo", "xho"],
+    ["How do I get a grant for my baby?", "isibonelelo", "eng"],
+
+    # Phrase 19:
+    ["Ingaba umntwana wam uzobane HIV?", "hiv", "xho"],
+    ["Will my baby have HIV?", "hiv", "eng"],
+
+    # Phrase 20:
+    ["Ndingancancisa ndine HIV?", "hiv_cancisa", "xho"],
+    ["Can I breastfeed with HIV?", "hiv_cancisa", "eng"],
+
+    # Phrase 21:
+    ["Umntwana wam unorhawuzelelo owenziwa yi-nappy", "nappy_rash", "xho"],
+    ["My baby has nappy rash", "nappy_rash", "eng"],
+
+    # Phrase 22:
+    ["Umntwana wam akatyi", "akatyi", "xho"],
+    ["My baby is not eating", "akatyi", "eng"],
+
+    # Phrase 23:
+    ["Ndingabelana ngesondo xa dikhulelwe?", "ngesondo", "xho"],
+    ["Can I have sex during pregnancy?", "ngesondo", "eng"],
+
+    # Phrase 24:
+    ["Ndingayenza I C-Section?", "c-section", "xho"],
+    ["Ndingayenza uqhaqho?", "c-section", "xho"],
+    ["Should I have a C Section?", "c-section", "eng"],
+
+]
+
+
