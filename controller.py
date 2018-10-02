a1362ca5c47c1154f58aade11e11d9014ecd3596
Carla Wilby <carla@zelda.org.za>
2018-10-02 23:14:56 +0200

    Added response generator and response on labelled list

---
+++ controller.py
@@ -0,0 +1,7 @@
+# Here poll for new messages
+
+# Need to get user's id somehow - initiate from app?
+
+# On new message received make FAQ matcher request
+
+# On FAQ matcher response use response_generator to get answer
